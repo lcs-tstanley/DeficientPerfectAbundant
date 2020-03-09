fa212a052be10819e29cb5ef4dcf53eb76159119
Trent Stanley <tstanley@lcs.on.ca>
2020-03-09 11:20:42 -0400

    Initial Commit

---
+++ DeficientPerfectAbundant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DeficientPerfectAbundant
+//
+//  Created by Stanley, Trent on 2020-03-09.
+//  Copyright © 2020 Stanley, Trent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
